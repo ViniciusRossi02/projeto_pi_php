b7d74310aa5e00281a91178a5619719d2ab5de55
unknown <rossivini1010@gmail.com>
2025-11-03 18:06:24 -0300

    Adicionando pages

---
+++ app/Views/dashBoard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>Rossi FutStore</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+
+<body>
+    
+    <header class="menu-bg">
+        <div class="menu">
+            <div class="menu-logo">
+                <a href="#">Rossi FutStore</a>
+            </div>
+                <nav class="menu-nav">
+                    <ul>
+                        <li> <a href="sobre">Listagem de usuario</a> </li>
+                        <li> <a href="produtos">Cadastro</a> </li>
+                        <li> <a href="preço">Procurar</a> </li>
+                        <li> <a href="qualidade">Qualidade</a> </li>
+                    </ul>
+                </nav>
+        </div>
+    </header>
+
+</body>
